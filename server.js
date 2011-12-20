2b45a7b018f998c2c0e135e06d4114654fb85cd4
benn0r <admin@benn0r.ch>
2011-12-20 21:50:23 +0100

    very simple server

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+var sys = require('util');
+var url = require('url');
+
+var port = 1005;
+var clients = [];
+var messages = [];
+
+var message = '';
+
+http.createServer(function (request, response) {
+	var urlparts = url.parse(request.url);
+	console.log(urlparts.pathname);
+	
+	switch (urlparts.pathname) {
+		case '/push':
+			// very very hawt, yay
+			message = urlparts.query.replace('message=', '');
+			console.log('pushed new message: ' + message);
+			
+			send();
+			
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.end('thx\n');
+			break;
+		case '/fetch':
+			clients.push(response);
+			console.log('client connected');
+			break;
+	}
+}).listen(port);
+
+function send() {
+	for (var i = 0; i < clients.length; i++) {
+		var client = clients[i];
+
+		client.writeHead(200, {'Content-Type': 'text/plain'});
+		client.end(message + '\n');
+	}
+}
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
