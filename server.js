14e3ba9493342f97fe5d4992a7906d7bbdfbd71a
benn0r <admin@benn0r.ch>
2011-12-20 22:07:13 +0100

    implementation of first simple client

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
+/**
+ * Chat Server
+ * 
+ * @author benn0r <admin@benn0r.ch>
+ * @since 2011/12/20
+ * @version 2011/12/20
+ */
+
+// Required modules
 var http = require('http');
 var sys = require('util');
 var url = require('url');
+var fs = require('fs');
 
-var port = 1005;
-var clients = [];
+var port = 1007; // server port
+var clients = []; // list with all connected clients
 var messages = [];
 
-var message = '';
+var message = ''; // active message
 
 http.createServer(function (request, response) {
 	var urlparts = url.parse(request.url);
 	console.log(urlparts.pathname);
 	
 	switch (urlparts.pathname) {
+		case '/client.html':
+			fs.readFile('./client.html', function (err, data) {
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.end(data);  
+			});
+
+			break;
+		case '/jquery.js':
+			fs.readFile('./jquery.js', function (err, data) {
+				response.writeHead(200, {'Content-Type': 'text/javascript'});
+				response.end(data);  
+			});
+
+			break;
 		case '/push':
 			// very very hawt, yay
 			message = urlparts.query.replace('message=', '');
-			console.log('pushed new message: ' + message);
+			console.log('client pushed new message: ' + message);
 			
 			send();
 			
@@ -24,12 +48,17 @@ http.createServer(function (request, response) {
 			response.end('thx\n');
 			break;
 		case '/fetch':
+			// register client
 			clients.push(response);
 			console.log('client connected');
 			break;
 	}
 }).listen(port);
 
+/**
+ * Sends a message to all connected clients and closes
+ * all the connections
+ */
 function send() {
 	for (var i = 0; i < clients.length; i++) {
 		var client = clients[i];
@@ -37,6 +66,9 @@ function send() {
 		client.writeHead(200, {'Content-Type': 'text/plain'});
 		client.end(message + '\n');
 	}
+	
+	// @todo good or bad? maybe bad
+	clients = [];
 }
 
 console.log('Server running at http://127.0.0.1:' + port + '/');
